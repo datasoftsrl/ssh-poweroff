55e15a119b1a9f92bf6656047005030114b33ee3
Riccardo Macoratti <r.macoratti@gmx.co.uk>
2016-11-21 23:30:08 +0100

    README.md: added installation part, with reminder on YAML configuration. install.py: installation script for Debian and Arch Linux.

--- README.md
+++ README.md
@@ -8,9 +8,25 @@ projectors.
 ## Requirements
 The project is exclusively tested on Debian 8 Jessie.
 
-- `Python` >= `2.5`
+- `Python` >= `3.5`
 - `systemd` >= `215`
 - `Flask` >= `0.11.1`
 - `gunicorn` >= `19.6.0`
 - `pexpect` >= `4.2.1`
 - `PyYAML` >= `3.11`
+
+## Installation
+To begin the installation, `su` into root account, `cd` to a writable folder 
+and launch the following command:
+
+`curl -sL https://gitlab.com/datasoftsrl/projector-poweroff/raw/master/install.py | python -`
+
+Do not forget to edit the configuration file.
+
+## Configuration
+Configuration is to be written in the YAML format.
+
+Config is read from a file located in the installation path (by default
+`/opt/projector-poweroff`) and a commented example is provided.
+
+NOTE: The YAML format does not accept tabs, so do not use them.
---
+++ install.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import sys, os, platform
+import shutil as sh, subprocess as sp
+from os import path
+
+PREFIX = path.dirname(path.realpath(__file__))
+
+PATH = '/tmp/projector-poweroff'
+REPO = 'http://lol'
+WD = path.join(PREFIX, 'projector-poweroff')
+
+if __name__ == '__main__':
+
+  if os.geteuid() == 0:
+    # only arch linux and debian/ubuntu supported in installer
+    distro = platform.linux_distribution()[0].lower()
+    if distro == 'arch':
+      sp.run(['pacman', '-S', '--noconfirm', 'python-pip git'],
+        stdin=sp.DEVNULL, stdout=sp.DEVNULL, stderr=sp.DEVNULL)
+    elif distro == 'ubunutu' or distro == 'debian':
+      sp.run(['apt-get', 'install', '-y', 'python3-pip git'],
+        stdin=sp.DEVNULL, stdout=sp.DEVNULL, stderr=sp.DEVNULL)
+    else:
+      print('Sorry, your distro is currently not supported with this installer.')
+
+    # installation commands (it will remove everything in 
+    sp.run(['pip3', 'install', 'flask pyyaml gunicorn pexpect'],
+      stdin=sp.DEVNULL, stdout=sp.DEVNULL, stderr=sp.DEVNULL)
+    sp.run(['git', 'clone', REPO, WD])
+    sp.run(['rm', '-rf', PATH],
+      stdin=sp.DEVNULL, stdout=sp.DEVNULL, stderr=sp.DEVNULL)
+    sh.copytree(WD, PATH)
+    sh.copy(path.join(PATH, 'ppoff.service'), '/etc/systemd/system')
+    sp.run(['systemctl', 'daemon-reload'],
+      stdin=sp.DEVNULL, stdout=sp.DEVNULL, stderr=sp.DEVNULL)
+    sp.run(['systemctl', 'enable', 'ppoff'],
+      stdin=sp.DEVNULL, stdout=sp.DEVNULL, stderr=sp.DEVNULL)
+    sp.run(['systemctl', 'start', 'ppoff'],
+      stdin=sp.DEVNULL, stdout=sp.DEVNULL, stderr=sp.DEVNULL)
+  else:
+    print('This script must be run as root.')
